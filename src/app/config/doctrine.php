be9a247df978d825698654c193a6deb19306b27e
Rogério Silva <ress.rogerio@gmail.com>
2020-09-02 11:00:15 -0300

    Update doctrine.php

--- src/app/config/doctrine.php
+++ src/app/config/doctrine.php
@@ -26,7 +26,7 @@ $odmConfig->setProxyNamespace('Proxies');
 $odmConfig->setHydratorDir(BASE_PATH . '/hydrators');
 $odmConfig->setHydratorNamespace('Hydrators');
 $odmConfig->setMetadataDriverImpl(AnnotationDriver::create(APP_PATH . '/documents'));
-$odmConfig->setDefaultDB('bookedplace_profile');
+$odmConfig->setDefaultDB('default_db');
 
 $dm = DocumentManager::create($odmClient, $odmConfig);
 
